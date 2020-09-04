d2787363b81a634592e0ed9561e10501c6633d4c
Alberto Robles <alberto.robles.rodriguez@ibm.com>
2020-09-04 00:00:02 -0500

    compatibility for podman with docker alias

--- src/docker.js
+++ src/docker.js
@@ -6,13 +6,13 @@ const Util = imports.misc.util;
 const ByteArray = imports.byteArray;
 
 var dockerCommandsToLabels = {
-  start: "Start",
-  restart: "Restart",
-  stop: "Stop",
-  pause: "Pause",
-  unpause: "Unpause",
-  exec: "Exec",
-  logs: "Logs"
+    start: "Start",
+    restart: "Restart",
+    stop: "Stop",
+    pause: "Pause",
+    unpause: "Unpause",
+    exec: "Exec",
+    logs: "Logs"
 };
 
 /**
@@ -26,12 +26,16 @@ var isDockerInstalled = () => !!GLib.find_program_in_path("docker");
  * @return {Boolean} whether current Linux user is in 'docker' group or not
  */
 var isUserInDockerGroup = () => {
-  const _userName = GLib.get_user_name();
-  let _userGroups = GLib.spawn_command_line_sync("groups " + _userName)[1].toString();
-  let _inDockerGroup = false;
-  if (_userGroups.match(/\sdocker[\s\n]/g)) _inDockerGroup = true; // Regex search for ' docker ' or ' docker' in Linux user's groups
+    const _userName = GLib.get_user_name();
+    let _userGroups = GLib.spawn_command_line_sync("groups " + _userName)[1].toString();
+    let _inDockerGroup = false;
+    if (_userGroups.match(/\sdocker[\s\n]/g)) _inDockerGroup = true; // Regex search for ' docker ' or ' docker' in Linux user's groups
 
-  return _inDockerGroup;
+    return _inDockerGroup;
+};
+
+var isPodmanInstalled = () => {
+    return !!GLib.find_program_in_path('podman') && !isDockerRunning();
 };
 
 /**
@@ -39,30 +43,29 @@ var isUserInDockerGroup = () => {
  * @return {Boolean} whether docker daemon is running or not
  */
 var isDockerRunning = () => {
-  const [res, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(
-    null,
-    ["/bin/ps", "cax"],
-    null,
-    0,
-    null
-  );
+    const [res, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(
+        null, ["/bin/ps", "cax"],
+        null,
+        0,
+        null
+    );
 
-  const outReader = new Gio.DataInputStream({
-    base_stream: new Gio.UnixInputStream({ fd: out_fd })
-  });
+    const outReader = new Gio.DataInputStream({
+        base_stream: new Gio.UnixInputStream({ fd: out_fd })
+    });
 
-  let dockerRunning = false;
-  let hasLine = true;
-  do {
-    const [out, size] = outReader.read_line(null);
-    if (out && ByteArray.toString(out).indexOf("docker") > -1) {
-      dockerRunning = true;
-    } else if (size <= 0) {
-      hasLine = false;
-    }
-  } while (!dockerRunning && hasLine);
+    let dockerRunning = false;
+    let hasLine = true;
+    do {
+        const [out, size] = outReader.read_line(null);
+        if (out && ByteArray.toString(out).indexOf("docker") > -1) {
+            dockerRunning = true;
+        } else if (size <= 0) {
+            hasLine = false;
+        }
+    } while (!dockerRunning && hasLine);
 
-  return dockerRunning;
+    return dockerRunning;
 };
 
 /**
@@ -70,32 +73,32 @@ var isDockerRunning = () => {
  * @return {Array} The array of containers as { project, name, status }
  */
 var getContainers = () => {
-  const [res, out, err, status] = GLib.spawn_command_line_sync(
-    "docker ps -a --format '{{.Names}},{{.Status}}'"
-  );
-  if (status !== 0) throw new Error("Error occurred when fetching containers");
+    const [res, out, err, status] = GLib.spawn_command_line_sync(
+        "docker ps -a --format '{{.Names}},{{.Status}}'"
+    );
+    if (status !== 0) throw new Error("Error occurred when fetching containers");
 
-  return String.fromCharCode
-    .apply(String, out)
-    .trim()
-    .split("\n")
-    .filter(string => string.length > 0)
-    .map(string => {
-      const values = string.split(",");
+    return String.fromCharCode
+        .apply(String, out)
+        .trim()
+        .split("\n")
+        .filter(string => string.length > 0)
+        .map(string => {
+            const values = string.split(",");
 
-      // Get 'docker-compose' project name for the container
-      let projectName = GLib.spawn_command_line_sync("docker inspect -f '{{index .Config.Labels \"com.docker.compose.project\"}}' " + values[0])[1].toString();
-      projectName = projectName.replace("\n", "");
-      projectName = projectName.toUpperCase();
-      projectName = projectName;
-      if (projectName != "" ) projectName = projectName + " ∘ ";
+            // Get 'docker-compose' project name for the container
+            let projectName = GLib.spawn_command_line_sync("docker inspect -f '{{index .Config.Labels \"com.docker.compose.project\"}}' " + values[0])[1].toString();
+            projectName = projectName.replace("\n", "");
+            projectName = projectName.toUpperCase();
+            projectName = projectName;
+            if (projectName != "") projectName = projectName + " ∘ ";
 
-      return {
-        project: projectName,
-        name: values[0],
-        status: values[1]
-      };
-    });
+            return {
+                project: projectName,
+                name: values[0],
+                status: values[1]
+            };
+        });
 };
 
 /**
@@ -104,74 +107,74 @@ var getContainers = () => {
  * @param {String} containerName The container
  * @param {Function} callback A callback that takes the status, command, and stdErr
  */
-var runCommand = async (command, containerName, callback) => {
-  var cmd = [""];
-  switch (command) {
-    case "exec":
-      cmd = [
-        "x-terminal-emulator",
-        "-e",
-        "bash",
-        "-c",      
-        "'docker exec -it " + containerName + " sh; exec $SHELL'"
-      ];
-      GLib.spawn_command_line_async(cmd.join(" "));
-      break;
-    case "logs":
-      cmd = [
-        "x-terminal-emulator",
-        "-e",
-        "bash",
-        "-c",
-        "'docker logs -f --tail 2000 " + containerName + "; exec $SHELL' "
-      ];
-      GLib.spawn_command_line_async(cmd.join(" "));
-      break;
-    default:
-      cmd = ["docker", command, containerName];
-      execCommand(cmd, callback);
-  }
+var runCommand = async(command, containerName, callback) => {
+    var cmd = [""];
+    switch (command) {
+        case "exec":
+            cmd = [
+                "x-terminal-emulator",
+                "-e",
+                "bash",
+                "-c",
+                "'docker exec -it " + containerName + " sh; exec $SHELL'"
+            ];
+            GLib.spawn_command_line_async(cmd.join(" "));
+            break;
+        case "logs":
+            cmd = [
+                "x-terminal-emulator",
+                "-e",
+                "bash",
+                "-c",
+                "'docker logs -f --tail 2000 " + containerName + "; exec $SHELL' "
+            ];
+            GLib.spawn_command_line_async(cmd.join(" "));
+            break;
+        default:
+            cmd = ["docker", command, containerName];
+            execCommand(cmd, callback);
+    }
 };
 
 async function execCommand(
-  argv,
-  callback /*(status, command, err) */,
-  cancellable = null
+    argv,
+    callback /*(status, command, err) */ ,
+    cancellable = null
 ) {
-  try {
-    // There is also a reusable Gio.SubprocessLauncher class available
-    let proc = new Gio.Subprocess({
-      argv: argv,
-      // There are also other types of flags for merging stdout/stderr,
-      // redirecting to /dev/null or inheriting the parent's pipes
-      flags: Gio.SubprocessFlags.STDOUT_PIPE
-    });
+    try {
+        // There is also a reusable Gio.SubprocessLauncher class available
+        let proc = new Gio.Subprocess({
+            argv: argv,
+            // There are also other types of flags for merging stdout/stderr,
+            // redirecting to /dev/null or inheriting the parent's pipes
+            flags: Gio.SubprocessFlags.STDOUT_PIPE
+        });
 
-    // Classes that implement GInitable must be initialized before use, but
-    // an alternative in this case is to use Gio.Subprocess.new(argv, flags)
-    //
-    // If the class implements GAsyncInitable then Class.new_async() could
-    // also be used and awaited in a Promise.
-    proc.init(null);
+        // Classes that implement GInitable must be initialized before use, but
+        // an alternative in this case is to use Gio.Subprocess.new(argv, flags)
+        //
+        // If the class implements GAsyncInitable then Class.new_async() could
+        // also be used and awaited in a Promise.
+        proc.init(null);
 
-    let stdout = await new Promise((resolve, reject) => {
-      // communicate_utf8() returns a string, communicate() returns a
-      // a GLib.Bytes and there are "headless" functions available as well
-      proc.communicate_utf8_async(null, cancellable, (proc, res) => {
-        let ok, stdout, stderr;
+        let stdout = await new Promise((resolve, reject) => {
+            // communicate_utf8() returns a string, communicate() returns a
+            // a GLib.Bytes and there are "headless" functions available as well
+            proc.communicate_utf8_async(null, cancellable, (proc, res) => {
+                let ok, stdout, stderr;
 
-        try {
-          [ok, stdout, stderr] = proc.communicate_utf8_finish(res);
-          callback && callback(ok, argv.join(" "), ok ? stdout : stderr);
-          resolve(stdout);
-        } catch (e) {
-          reject(e);
-        }
-      });
-    });
+                try {
+                    [ok, stdout, stderr] = proc.communicate_utf8_finish(res);
+                    callback && callback(ok, argv.join(" "), ok ? stdout : stderr);
+                    resolve(stdout);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        });
 
-    return stdout;
-  } catch (e) {
-    logError(e);
-  }
+        return stdout;
+    } catch (e) {
+        logError(e);
+    }
 }
--- src/dockerMenu.js
+++ src/dockerMenu.js
@@ -12,69 +12,69 @@ const GObject = imports.gi.GObject;
 
 // Docker icon as panel menu
 var DockerMenu = GObject.registerClass(
-  class DockerMenu extends panelMenu.Button {
-    _init(menuAlignment, nameText) {
-      super._init(menuAlignment, nameText);
-      
-      // Custom Docker icon as menu button
-      const gioIcon = (name = "docker-symbolic") => Gio.icon_new_for_string(Me.path + "/icons/" + name + ".svg");
-      const panelIcon = (name = "docker-symbolic", styleClass = "system-status-icon") => new St.Icon({ gicon: gioIcon(name), style_class: styleClass, icon_size: "16" });
-      this.actor.add_child(panelIcon("docker-symbolic",));
-      this.connect("button_press_event", this._refreshMenu.bind(this));
-      this._renderMenu();
-    }
+    class DockerMenu extends panelMenu.Button {
+        _init(menuAlignment, nameText) {
+            super._init(menuAlignment, nameText);
 
-    // Refresh  the menu everytime the user click on it
-    // It allows to have up-to-date information on docker containers
-    _refreshMenu() {
-      if (this.menu.isOpen) {
-        this.menu.removeAll();
-        this._renderMenu();
-      }
-    }
-    
-    // Show docker menu icon only if installed, append docker containers, and manageable with current user without 'sudo'
-    _renderMenu() {
-      if (Docker.isDockerInstalled()) {
-        if (Docker.isUserInDockerGroup()) {
-          if (Docker.isDockerRunning()) {
-            this._feedMenu();
-          } else {
-            let errMsg = _("Please start your Docker service first!\n(Seems Docker daemon not started yet.)");
-            this.menu.addMenuItem(new PopupMenuItem(errMsg));
-            log(errMsg);
-          }
-        } else {
-          let errMsg = _("Please put your Linux user into `docker` group first!\n(Seems not in that yet.)");
-          this.menu.addMenuItem(new PopupMenuItem(errMsg));
-          log(errMsg);
+            // Custom Docker icon as menu button
+            const gioIcon = (name = "docker-symbolic") => Gio.icon_new_for_string(Me.path + "/icons/" + name + ".svg");
+            const panelIcon = (name = "docker-symbolic", styleClass = "system-status-icon") => new St.Icon({ gicon: gioIcon(name), style_class: styleClass, icon_size: "16" });
+            this.actor.add_child(panelIcon("docker-symbolic", ));
+            this.connect("button_press_event", this._refreshMenu.bind(this));
+            this._renderMenu();
         }
-      } else {
-        let errMsg = _("Please properly install Docker service first!\n(Seems Docker binary not found in PATH yet.)");
-        this.menu.addMenuItem(new PopupMenuItem(errMsg));
-        log(errMsg);
-      }
-      this.show();
-    }
-    
-    // Append containers to menu
-    _feedMenu() {
-      try {
-        const containers = Docker.getContainers();
-        if (containers.length > 0) {
-          containers.forEach(container => {
-            const subMenu = new DockerSubMenu(container.project, container.name, container.status);
-            this.menu.addMenuItem(subMenu);
-          });
-        } else {
-          this.menu.addMenuItem(new PopupMenuItem("No containers detected"));
+
+        // Refresh  the menu everytime the user click on it
+        // It allows to have up-to-date information on docker containers
+        _refreshMenu() {
+            if (this.menu.isOpen) {
+                this.menu.removeAll();
+                this._renderMenu();
+            }
         }
-      } catch (err) {
-        const errMsg = "Error occurred when fetching containers";
-        this.menu.addMenuItem(new PopupMenuItem(errMsg));
-        log(errMsg);
-        log(err);
-      }
+
+        // Show docker menu icon only if installed, append docker containers, and manageable with current user without 'sudo'
+        _renderMenu() {
+            if (Docker.isDockerInstalled() || Docker.isPodmanInstalled()) {
+                if (Docker.isUserInDockerGroup() || Docker.isPodmanInstalled()) {
+                    if (Docker.isDockerRunning() || Docker.isPodmanInstalled()) {
+                        this._feedMenu();
+                    } else {
+                        let errMsg = _("Please start your Docker service first!\n(Seems Docker daemon not started yet.)");
+                        this.menu.addMenuItem(new PopupMenuItem(errMsg));
+                        log(errMsg);
+                    }
+                } else {
+                    let errMsg = _("Please put your Linux user into `docker` group first!\n(Seems not in that yet.)");
+                    this.menu.addMenuItem(new PopupMenuItem(errMsg));
+                    log(errMsg);
+                }
+            } else {
+                let errMsg = _("Please properly install Docker service first!\n(Seems Docker binary not found in PATH yet.)");
+                this.menu.addMenuItem(new PopupMenuItem(errMsg));
+                log(errMsg);
+            }
+            this.show();
+        }
+
+        // Append containers to menu
+        _feedMenu() {
+            try {
+                const containers = Docker.getContainers();
+                if (containers.length > 0) {
+                    containers.forEach(container => {
+                        const subMenu = new DockerSubMenu(container.project, container.name, container.status);
+                        this.menu.addMenuItem(subMenu);
+                    });
+                } else {
+                    this.menu.addMenuItem(new PopupMenuItem("No containers detected"));
+                }
+            } catch (err) {
+                const errMsg = "Error occurred when fetching containers";
+                this.menu.addMenuItem(new PopupMenuItem(errMsg));
+                log(errMsg);
+                log(err);
+            }
+        }
     }
-  }
 );
